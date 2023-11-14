cc19b06d62c700e9c60c7ce432308f86b72a541c
acer <you@example.com>
2023-11-14 19:09:31 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+// destructuring assignment
+
+
+// array
+
+// let cars = ['Cobalt', 'Spark', 'Malibu']
+// let [a, ...rest] = cars // har bir index da turgan itemni indexlarini biror bir qiymatga tenglab olw
+// console.log(rest) // va index nomi bn caqrw
+
+
+
+// object
+
+// let obj = {
+//     name: 'Umar',
+//     age: 20,
+//     lastName: 'Yunusov'
+// }
+// let {name, age, lastName} = obj
+// console.log(name)
+
+
+// String
+
+// let someThing = 'Mustaqillik'
+// let [a,b,c,d, ...rest] = someThing
+// console.log(rest)
+
+
+// 1.D
+// 2.C
+// 3.A        // B
+// 4.A
+// 5.D        // A
+
+
